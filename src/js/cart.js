e92ee19361a1c639fefc5235ae4bb8a9452397eb
Travis Nelson <tnelson4@byupathway.edu>
2026-01-17 06:19:26 -0700

    Implement remove item functionality in cart and style remove button

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,19 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Add event listeners to remove buttons
+  document.querySelectorAll(".cart-card__remove").forEach((button) => {
+    button.addEventListener("click", removeFromCart);
+  });
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
+  <span class="cart-card__remove" data-id="${item.Id}">X</span>
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -23,6 +29,17 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+function removeFromCart(e) {
+  const productId = e.target.dataset.id;
+  let cartItems = getLocalStorage("so-cart");
+  const index = cartItems.findIndex((item) => item.Id === productId);
+  if (index !== -1) {
+    cartItems.splice(index, 1);
+  }
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
 }
 
 renderCartContents();
