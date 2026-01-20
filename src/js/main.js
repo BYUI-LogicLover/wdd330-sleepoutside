44c918fed6ec1ce7a0f47e2e18b3631cd13f290f
issacawuah <email>
2026-01-20 20:01:03 +0000

    fix: add responsive product image sizing

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
+const dataSource = new ProductData('tents');
+const element = document.querySelector('.product-list');
 
 // Only display products that have detail pages
-const productIdsWithDetailPages = ["880RR", "985RF", "985PR", "344YJ"];
+const productIdsWithDetailPages = ['880RR', '985RF', '985PR', '344YJ'];
 
 const productList = new ProductList(
-  "Tents",
+  'Tents',
   dataSource,
   element,
   productIdsWithDetailPages,
